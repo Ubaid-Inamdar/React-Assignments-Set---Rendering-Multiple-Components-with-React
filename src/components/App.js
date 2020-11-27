655c0ca45837d1e047349e3eb5b1a8c510bb4730
Ubaid-Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-11-28 00:48:32 +0530

    Multiple Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name"> Electronics Renting Application </h1>
+      <p data-ns-test="project-description">
+        {" "}
+        An application where you can rent any Electronic gadgets like
+        Laptop,Camera,Mobile,Smart devices,etc
+      </p>
+    </div>
+  );
+};
 
 export default App;
